a45996a4e9503ef4861e1dad97b0e70ae464a824
王佳灿 <wjc0216wjc>
2017-02-01 22:04:02 +0800

    4.3章练习题_枚举求最大公约数

--- mooc_036.c
+++ mooc_036.c
@@ -2,7 +2,35 @@
 
 int main()
 {
-	printf("%d", 1 / 10);
+	//初始化变量
+	int n;
+	int m;
+	int max;
+	int i;
+	int ret;
+	
+	//输入数值
+	printf("请输入需要求公约数的值：");
+	scanf("%d %d", &n, &m);
+	
+	//确认最大的数值
+	if (n > m){
+		max = n;
+	} else {
+		max = m;
+	}
+	
+	//枚举最大公约数
+	for (i = 1; i < max ; i++){
+		if (n % i == 0){
+			if (m % i == 0){
+				ret = i;
+			}
+		}
+	}
+	
+	//输出最大公约数
+	printf("%d与%d的最大公约数为：%d", n, m, ret);
 	
 	return 0;
 }
