daf57f5cc4b8760d7de62f137125f46f826cd873
王佳灿 <wangjiacan0216@gmail.com>
2017-02-11 20:16:12 +0800

    单元测试调试代码

---
+++ mooc_039.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*单元测试调试代码*/
+int main()
+{
+//	int i = 6;
+//	
+//	do {
+//		printf("%d", i--);
+//		
+//	} while(0);
+//	int i ;
+//	for (i = 0; i < 10; i++){
+//		printf("%d\n", i);
+//	}
+	printf("%d", 17%4/8);
+	return 0;
+}
